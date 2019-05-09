058d59f6ba9130f3b4c79229cd66c47b071d9d11
Michel Lang <michellang@gmail.com>
2019-05-09 22:08:08 +0200

    ...

--- tests/testthat/test_PredictionSurv.R
+++ tests/testthat/test_PredictionSurv.R
@@ -3,18 +3,15 @@ context("PredictionSurv")
 test_that("Construction", {
   task = mlr_tasks$get("lung")
   p = PredictionSurv$new(task = task, risk = runif(task$nrow))
-  expect_prediction(p)
-  # expect_prediction_surv(p)
+  expect_prediction_surv(p)
 })
 
 test_that("Internally constructed Prediction", {
   task = mlr_tasks$get("lung")
-  # lrn = mlr_learners$get("surv.featureless")
-  # lrn$predict_type = "prob"
-  # e = Experiment$new(task, lrn)$train()$predict()
-  # p = e$prediction
-  # expect_prediction(p)
-  # expect_prediction_classif(p, task = task)
+  lrn = mlr_learners$get("surv.rpart")
+  e = Experiment$new(task, lrn)$train()$predict()
+  p = e$prediction
+  expect_prediction_surv(p)
 })
 
 test_that("rbind", {
@@ -25,10 +22,8 @@ test_that("rbind", {
   preds = map(rr$experiments(), "prediction")
 
   pred = do.call(rbind, map(rr$experiments(), "prediction"))
-  expect_prediction(pred)
-  # expect_prediction_surv(pred)
+  expect_prediction_surv(pred)
 
   dt = as.data.table(pred)
   expect_data_table(dt, nrow = task$nrow, ncol = 4L, any.missing = FALSE)
-
 })

079cc047991b123b6829125da414b905aadcd295
Michel Lang <michellang@gmail.com>
2019-07-14 15:51:28 +0200

    fix some partial matching issues

--- tests/testthat/test_PredictionSurv.R
+++ tests/testthat/test_PredictionSurv.R
@@ -24,5 +24,5 @@ test_that("c", {
   expect_prediction_surv(pred)
 
   dt = as.data.table(pred)
-  expect_data_table(dt, nrow = task$nrow, ncol = 4L, any.missing = FALSE)
+  expect_data_table(dt, nrows = task$nrow, ncols = 4L, any.missing = FALSE)
 })
